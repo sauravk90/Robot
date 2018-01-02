d222b8a4c10619b132126a23c005b6b939f04590
sauravk90 <stonewarrior080>
2018-01-02 20:54:56 +0530

    Updated poms

--- framework/testBase/TestException.java
+++
@@ -1,8 +0,0 @@
-package testBase;
-
-public class TestException extends RuntimeException {
-
-    public TestException(String message) {
-        super(message);
-    }
-}
--- framework/utilities/Constants.java
+++
@@ -1,6 +0,0 @@
-package utilities;
-
-public class Constants {
-
-    public static final String URL = "https://www.paytmmall.com";
-}
